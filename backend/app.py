dd280e9d0cbbea2fc4c87828959943dd47cb58da
sreenathsuu <srsuu89@gmail.com>
2026-06-03 21:56:20 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -272,11 +272,10 @@ def home():
         "status": "online",
         "service": "SecureVault API"
     })
-    if __name__ == "__main__":
-    app.run(
+   if __name__ == "__main__":
+       app.run(
         debug=False,
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 5000))
     )
-
     
